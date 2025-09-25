2143a994d9efe33668fbd5ef5f5979f316ee228f
Meet Nakrani <nakranimeet2005@gmail.com>
2025-09-25 10:42:08 +0530

    Next Js

---
+++ src/app/product/page.tsx
@@ -0,0 +1,9 @@
+export default function ProductList() {
+  return (
+    <>
+      <h1>Welcome to the product page!</h1>;<h1>Product 1</h1>
+      <h1>Product 2</h1>
+      <h1>Product 3</h1>
+    </>
+  );
+}
